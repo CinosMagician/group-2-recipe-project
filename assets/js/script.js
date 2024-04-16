7fb11d5bc5ab899c8499f8fac80da91befae6677
Lachlan <lachyhughes@bigpond.com>
2024-04-16 19:22:53 +0930

    added a function to display the instructions onto the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 let userQuery = "tomato";
 let dish = 'pizza';
-let 
 
 function getRecipeList(item) {
   let apiRecipeUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${item.replaceAll(' ', '_')}`;
@@ -61,6 +60,23 @@ function getRecipe (mealId) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data.meals[0].strInstructions);
+
+          // Code for placing the instructions on the page
+          const instructionsArea = document.getElementById("instructionsList");
+          if (instructionsArea) {
+            instructionsArea.innerHTML = '';
+
+            let splitLines = data.meals[0].strInstructions.match(/[^\r\n]+/g);
+            for (const line of splitLines) {
+              console.log(line);
+              const listItem = document.createElement('li');
+              listItem.textContent = line;
+              instructionsArea.appendChild(listItem);
+            }
+          } else {
+            console.error("Element with ID 'instructionsList' not found.");
+
+          }
         });
       } else {
         alert(`Error: ${response.statusText}`);
